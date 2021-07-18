88549189cb1a32eb59e4945b629fc6300b76781e
nelidita <nelidavalero06@gmail.com>
2021-07-18 14:04:06 -0500

    views de productos agregados

--- main.js
+++ main.js
@@ -6,15 +6,61 @@
 // const fetch = require("node-fetch")
 
 // const products = fetch("./data/products.json")
-// console.log(products)
-
-const products = () => {
-    fetch("./data/products.json")
-        .then ( response => response.json()) //Indico en que formato queremos ver nuestra data
-        .then ( response => (console.log(response)))
-        .catch ( error => console.log(error))
+// console.log(data)
+const viewProduct = (product) => {
+    const divProduct = `
+        <div class= "product" id= "${product.id}">
+            <div class= "imgProduct">
+                <img src= "./assets/p1.png" atl="Imagen Producto">
+            </div>
+            <div class= "infoProduct">
+                <p class="nameProduct">${product.name}</p>
+                <p>${product.price}</p>
+                <p>Cantidad: ${product.quantity}</p>
+            </div>
+            <div class = "profile">
+                <img src="./assets/profile.png" alt ="profile"/>
+            </div>
+            <button class="button">Agregar</button>
+        </div>
+    `
+    return divProduct;
 }
-products();
 
-const dataProducts = products()
-console.log(dataProducts)
+
+const viewListProducts = (listProducts) => {
+    let divListProducts = '';
+    listProducts.forEach( product => {
+      divListProducts += viewProduct(product);
+    });
+  
+    return divListProducts;
+  };
+
+const dataProducts = () => {
+    return fetch("./data/products.json")
+        .then ( response => response.json()) //Indico en que fgit ormato queremos ver nuestra data
+        .then ( response => {
+            const objectProducts = response.products
+            // console.log(objectProducts)
+            // objectProducts.forEach( product => {
+                document.getElementById("products").innerHTML = viewListProducts(objectProducts)
+                // console.log(product.name)
+            // });
+        })
+        .catch ( error => console.log(error))
+};
+
+dataProducts();
+
+// const productsList = () => {
+    // console.log(dataProducts)
+    // dataProducts().then( products => products.forEach( product => {
+    //     console.log(product.name)
+    // }))
+// } 
+
+// productsList();
+
+
+
